53eb2e9277723074b7984825acfe6dd8783ec1bf
sireenyadav <sireenyadav@gmail.com>
2026-08-21 22:15:05 +0900

    fix(build): configure maven repository mirrors and http retry handlers to eliminate 403 transient rate limits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://repo1.maven.org/maven2/") }
     }
 }
 dependencyResolutionManagement {
@@ -10,6 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://repo1.maven.org/maven2/") }
+        maven { url = uri("https://jitpack.io") }
         maven { url = uri("https://chaquo.com/maven") }
     }
 }
